acad4b72aa7dcfaa64c762f36879b8e88fe25719
waffwaff1 <31948220+waffwaff1@users.noreply.github.com>
2017-09-15 10:01:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 app.get('/article-one', function (req, res) {
   res.send("article one here");
 });
